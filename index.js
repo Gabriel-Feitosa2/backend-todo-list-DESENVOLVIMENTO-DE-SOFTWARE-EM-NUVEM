2f708cdbc894b2b5b355fdcca9ca42f3461070cc
Gabriel Feitosa <gabrielfeitosa4228@gmail.com>
2024-09-14 19:30:38 -0300

    change in cors

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const app = express();
 
 app.use(express.json()); // Para lidar com requisições JSON
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 // Criar uma nova tarefa
 app.post("/todos", async (req, res) => {
   const { title, description } = req.body;
